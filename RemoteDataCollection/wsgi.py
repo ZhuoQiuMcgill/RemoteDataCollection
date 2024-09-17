b71c65fbb9a2573cbc447ac1bc02fca46d9bf160
ZhuoQiuMcgill <81338304+ZhuoQiuMcgill@users.noreply.github.com>
2024-09-17 13:49:36 -0400

    rename project

--- RemoteDataCollection/wsgi.py
+++ RemoteDataCollection/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RemoteDataCollection project.
+WSGI config for RomPlatform project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RemoteDataCollection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RomPlatform.settings')
 
 application = get_wsgi_application()
